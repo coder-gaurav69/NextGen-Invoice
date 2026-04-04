c158d29e5b50f82339294069a6da400540c52b0e
coder-gaurav69 <gauravchand222@gmail.com>
2026-04-05 00:01:03 +0530

    added

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ import express from 'express';
 import puppeteer from 'puppeteer';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
 import { PDFDocument, rgb, StandardFonts } from 'pdf-lib';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const envFileName = process.env.NODE_ENV === 'production' ? '.env.production' : '.env';
+dotenv.config({ path: path.join(__dirname, envFileName) });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
